15b95e088b6c9d55509bf58d8ba7a80dfae1d609
votro <hieuvt2309@gmail.com>
2025-10-27 18:05:57 +0700

    Tile

--- Assets/Scripts/Player/FollowPlayerUI.cs
+++ Assets/Scripts/Player/FollowPlayerUI.cs
@@ -5,6 +5,11 @@ public class FollowPlayerUI : MonoBehaviour
     public Transform player;
     public Vector3 offset = new Vector3(0, 0, 0);
 
+    void Awake()
+    {
+        gameObject.SetActive(true);
+    }
+
     void LateUpdate()
     {
         if (player != null)
